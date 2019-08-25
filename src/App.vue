6adc5914749886e4dd7d6f323af4dc12668e94b0
SAI <2578443177@qq.com>
2019-08-25 22:29:33 +0800

    new

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,101 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="warp" v-drag>
+        <div id="container">
+            <v-header></v-header>
+        </div>
     </div>
-    <router-view/>
-  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+    import vheader from '@/components/header.vue';
+    import {
+        mapState,
+        mapMutations,
+        mapActions
+    } from 'vuex';
+    export default {
+        name: 'game',
+        components: {
+            "v-header": vheader,
+        },
+        data() {
+            return {
+
+            }
+        },
+        created() {
+        },
+        mounted() {
+        },
+        computed: {
+
+        },
+        methods: {
+
+        },
+        directives: {
+            //拖动
+            drag: {
+                bind: function (el) {
+                    let node = el;   //获取当前元素
+                    node.onmousedown = (e) => {
+                        //算出鼠标相对元素的位置
+                        let disX = e.clientX - node.offsetLeft;
+                        let disY = e.clientY - node.offsetTop;
+
+                        document.onmousemove = (e) => {
+                            //用鼠标的位置减去鼠标相对元素的位置，得到元素的位置
+                            let left = e.clientX - disX;
+                            let top = e.clientY - disY;
+
+                            //移动当前元素
+                            node.style.left = left + 'px';
+                            node.style.top = top + 'px';
+                        };
+                        document.onmouseup = (e) => {
+                            document.onmousemove = null;
+                            document.onmouseup = null;
+                        };
+                    };
+                }
+            }
+        }
     }
-  }
-}
+</script>
+
+<style lang="scss">
+    //阿里图标
+    @import url("http://at.alicdn.com/t/font_586765_iustpfl9kap.css");
+    @import "../public/scss/animation.scss";
+    @import "../public/scss/ui.scss";
+
+    #warp {
+        position: absolute;
+        top: 5%;
+        left: calc(50% - 530px);
+        width: 1060px;
+        height: 700px;
+        border-radius: 20px;
+        box-shadow: 10px 10px 50px gray;
+        transition-duration: all 0.5s;
+        cursor: default;
+        color: rgba(255, 255, 255, 0.8);
+        background: #686669;
+
+        &.full {
+            @include fullscreen;
+        }
+
+        #container {
+            width: 100%;
+            height: 100%;
+            background: linear-gradient(
+                135deg,
+                rgba(186, 67, 168, 0.38) 0%,
+                rgba(233, 79, 205, 0.17) 61%,
+                rgba(234, 79, 206, 0.19) 62%,
+                rgba(83, 123, 173, 0.8) 100%
+            );
+        }
+    }
 </style>
---
+++ src/components/header.vue
@@ -0,0 +1,87 @@
+<template>
+    <div id="header">
+        <div class="logo"></div>
+        <div class="header-menu">
+            <div class="search-bar">
+                <input class="search" placeholder="搜索音乐，MV..." />
+            </div>
+            <div class="icons">
+                <div class="user">
+                    <div class="user-avatar sm">
+                        <i class="lg iconfont icon-user"></i>
+                    </div>
+                    <span class="user-nickname">欢迎！</span>
+                </div>
+                <i class="lg iconfont icon-skin"></i>
+                <i class="lg iconfont icon-setting"></i>
+                <i class="lg iconfont icon-full"></i>
+            </div>
+        </div>
+    </div>
+</template><script>export default {
+        name: 'vheader'
+
+    }
+
+</script><style lang="scss"scoped>
+    #header {
+        width: 100%;
+        height: 60px;
+
+        /*---------------导航栏------------------*/
+
+        .logo,
+        .header-menu {
+            height: 100%;
+        }
+
+        .logo {
+            float: left;
+            margin-left: 30px;
+            width: 200px;
+            height: 40px;
+            background: url("../../public/img/logo/logo.png");
+            background-repeat: no-repeat;
+        }
+
+        .header-menu {
+            font-size: 20px;
+            width: calc(100% - 246px);
+
+            .search-bar,
+            .icons {
+                height: 100%;
+            }
+
+            .search-bar {
+                .search {
+                    padding: 5px;
+                    width: 100%;
+                    height: 30px;
+                    border-radius: 1em;
+                }
+            }
+
+            .icons {
+                width: 280px;
+
+                .user {
+                    overflow: hidden;
+                    width: 120px;
+                    text-overflow: ellipsis;
+                    white-space: nowrap;
+                    font-size: 15px;
+                    line-height: 32px;
+
+                    .user-avatar {
+                        float: left;
+                    }
+
+                    .user-nickname {
+                        margin-left: 10px;
+                    }
+                }
+            }
+        }
+    }
+</style>
